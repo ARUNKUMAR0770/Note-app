1c225c5b99b2c4e6cfbac2ed5be84eeff636390b
ARUNKUMAR <arunotk07@gmail.com>
2026-04-30 00:05:16 +0530

    routing fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
-import Navbar from './pages/Navbar'
-import { Home } from './pages/Home'
-import { Login } from './pages/Login'
-import Account from './pages/Account'
-import API from './api/axios'
-import PageLoader from './components/PageLoader'
-import { Toaster, toast } from 'react-hot-toast'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import Account from './pages/Account';
 import Dashboard from './pages/Dashboard';
+import API from './api/axios';
+import PageLoader from './components/PageLoader';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+};
 
+const AppContent = () => {
   const [AuthUser, setAuthUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const Navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const checkAuth = async () => {
-      setIsLoading(true)
       try {
         const res = await API.get("/auth/check", { withCredentials: true });
         setAuthUser(res.data);
-        Navigate("/dashboard");
       } catch (error) {
-        console.log("Error:", error.response?.data || error.message);
         setAuthUser(null);
-        setIsLoading(false)
+      } finally {
+        setIsLoading(false);
       }
-      setIsLoading(false)
     };
 
     checkAuth();
   }, []);
 
-  useEffect(() => {
-  }, [AuthUser]);
-
-  if (isLoading) {
-    return <PageLoader />
-  }
+  if (isLoading) return <PageLoader />;
 
   return (
-    <BrowserRouter>
+    <>
       <Navbar name={AuthUser?.username} setAuthUser={setAuthUser} setIsLoading={setIsLoading} />
+
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/login" element={AuthUser ? <Dashboard /> : <Login setAuthUser={setAuthUser} setIsLoading={setIsLoading} />} />
+
         <Route path="/newaccount" element={AuthUser ? <Dashboard /> : <Account setIsLoading={setIsLoading} />} />
+
         <Route path="/dashboard" element={AuthUser ? <Dashboard setIsLoading={setIsLoading} /> : <Login setAuthUser={setAuthUser} />} />
       </Routes>
-      <Toaster />
-    </BrowserRouter>
-  )
-}
 
-export default App
+      <Toaster />
+    </>
+  );
+};
+
+export default App;
